a594f7908904fbf75d6cb3d9338e19ef9e3fb291
George Melissopoulos <melissopoulos.geo@gmail.com>
2024-01-29 17:02:49 +0200

    Changes on Navbar in order to logout from the app

--- frontend/my-todo-app/src/app/components/Navbar.js
+++ frontend/my-todo-app/src/app/components/Navbar.js
@@ -2,9 +2,35 @@
 
 import Link from "next/link";
 import { useAuth } from "../../AuthContext";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, setIsLoggedIn } = useAuth();
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/auth/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.status === 200) {
+        setIsLoggedIn(false);
+        router.refresh();
+        router.push("/");
+      } else {
+        // Handle any errors
+        console.error("Failed to log out:", data.message);
+      }
+    } catch (error) {
+      console.error("Failed to log out:", error);
+    }
+  };
 
   return (
     <nav>
@@ -14,7 +40,9 @@ export default function Navbar() {
           <Link href="/">Dashboard</Link>
           <Link href="/todos">Todos</Link>
           <Link href="/todos/create">Create </Link>
-          <Link href="/login">Logout </Link>
+          <Link href="/" onClick={logout}>
+            Logout
+          </Link>
         </>
       ) : (
         <>
